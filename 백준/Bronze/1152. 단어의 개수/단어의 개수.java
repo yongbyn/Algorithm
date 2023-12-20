60817cab54536b6fffd137f024aa6ebf6d16c450
yongbyn <44955172+yongbyn@users.noreply.github.com>
2023-12-20 17:45:18 +0900

    Update 단어의 개수.java
    실제 풀이

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,24 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // scanner 객체 생성
+        String A = input.nextLine(); // 입력받은 문자열 저장
+       
+        A = A.trim();        //양쪽 공백 제거
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] arr = br.readLine().split(" ");
-
-        if(arr.length == 0){
-            System.out.print(0);
-            System.exit(0);
-        }
-
-        if(arr[0] == "") {
-            System.out.println(arr.length - 1);
-        }else{
-            System.out.println(arr.length);
-        }
-
-    }
+        if (A.length() == 0) { // 공백이었을 경우 "" => 길이 0
+            System.out.println(0);
+            }else{
+            String[] txt = A.split(" "); // 문자열 공백으로 자르기
+            System.out.println(txt.length);
+            }
+   }
 }
