d2f75e9349d25631987a854010206deb3212e70a
yongbyn <44955172+yongbyn@users.noreply.github.com>
2024-02-16 10:33:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver III/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver III/11726. 2×n 타일링/2×n 타일링.java
@@ -17,15 +17,13 @@ public class Main{
 
         n = Integer.parseInt(br.readLine());
 
-        dp = new int[n+1];
+        dp = new int[n+2];
 
-        dp[0] = 1;
         dp[1] = 1;
-        
-
-            for (int i=2; i<n+1;i++){
-                dp[i] = (dp[i-1]+dp[i-2]) % 10007;
-            }
+        dp[2] = 2;
+        for (int i=3; i<n+1;i++){
+            dp[i] = (dp[i-1]+dp[i-2]) % 10007;
+        }
 
         System.out.println(dp[n]);
     }
