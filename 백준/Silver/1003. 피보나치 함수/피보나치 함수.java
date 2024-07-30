12b5851a6421f34d598dfdb3e74429a4e60785cb
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-07-30 21:35:18 +0900

    [Silver III] Title: 피보나치 함수, Time: 248 ms, Memory: 18432 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        int[][] dp = new int[41][2];
+        
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        
+        for (int i = 2; i < 41; i++){
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        }
+        
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+        
+        sc.close();
+    }
+}
