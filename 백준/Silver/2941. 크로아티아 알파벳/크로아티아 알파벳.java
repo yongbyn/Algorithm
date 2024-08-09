0df0d368d98ab2806cd0ee153697bb09ba06d218
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-08-09 15:48:56 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -6,20 +6,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        int length = str.length();
-        
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '-') length--;
-            else if (str.charAt(i) == 'j' && i >= 1) {
-                if (str.charAt(i-1) == 'l' || str.charAt(i-1) == 'n') length--;
-            }
-            else if(str.charAt(i) == '=' && i >= 1) {
-                if (str.charAt(i-1) == 'c' || str.charAt(i-1) == 's' || str.charAt(i-1) == 'z') length--;
-                if (i>=2) {
-                    if (str.charAt(i-2)== 'd' &&  str.charAt(i-1) == 'z') length--;
-                }
-            }
+        String[] croatiaAlphabets = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+            
+        for (String alphabet : croatiaAlphabets) {
+            str = str.replace(alphabet,"*");
         }
-        System.out.println(length);
+        System.out.println(str.length());
     }
 }
