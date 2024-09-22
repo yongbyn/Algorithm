a8d887844fc159254739dddee40606eb9a450048
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-09-22 16:59:05 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 960 ms, Memory: 173876 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -4,30 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        // 숫자 카드를 저장할 HashMap
-        HashMap<Integer, Integer> cardMap = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 가지고 있는 카드의 개수를 카운팅하여 HashMap에 저장
+        HashMap<Integer, Integer> cardHM = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            int card = Integer.parseInt(st.nextToken());
-            // 있으면 있는 개수에 +1, 없으면 0+1
-            cardMap.put(card, cardMap.getOrDefault(card, 0) + 1);
+            int key = Integer.parseInt(st.nextToken());
+            cardHM.put(key, cardHM.getOrDefault(key, 0) + 1);
         }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        // 찾으려는 카드가 있으면 그 개수를, 없으면 0을 출력
         for (int j = 0; j < M; j++) {
-            int sample = Integer.parseInt(st.nextToken());
-            sb.append(cardMap.getOrDefault(sample, 0)).append(" ");
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(cardHM.getOrDefault(key, 0)).append(" ");
         }
-
-        // 결과 출력
         System.out.println(sb.toString().trim());
     }
 }
