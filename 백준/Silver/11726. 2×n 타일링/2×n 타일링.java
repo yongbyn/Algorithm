669499a85cafbbfcc7496d40ee0b7e6b8bbc7952
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-10-01 22:14:52 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,30 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
-
-public class Main{
-
-    static int n;
-
-    static int[] dp;
-
+public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-
-        dp = new int[n+2];
-
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for (int i=3; i<n+1;i++){
-            dp[i] = (dp[i-1]+dp[i-2]) % 10007;
+        
+        for (int i = 2; i < n+1; i++) {
+            dp[i] = (dp[i-2] + dp[i-1])%10007;
         }
-
+        
         System.out.println(dp[n]);
     }
 }
