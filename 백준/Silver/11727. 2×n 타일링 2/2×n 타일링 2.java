47fdfd05a732596fb89f791e3686b9e25888d43c
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-10-11 20:24:03 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -9,15 +9,12 @@ public class Main {
 
 		int[] dp = new int[n+1];
 
+        dp[0] = 1;
 		dp[1] = 1;
 
 		if (n > 1) {
 			for (int i = 2; i < n+1; i++) {
-				if (i%2 == 0) {
-					dp[i] = (dp[i - 1] * 2 + 1) % 10007;
-				} else {
-					dp[i] = (dp[i - 1] * 2 - 1) % 10007;
-				}
+				dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
 			}
 		}
 
