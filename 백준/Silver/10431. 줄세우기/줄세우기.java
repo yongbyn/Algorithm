34a982d9d469e22550ae85627d0310929c338bde
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-10-21 10:36:11 +0900

    [Silver V] Title: 줄세우기, Time: 228 ms, Memory: 19632 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int P = Integer.parseInt(st.nextToken());
+        int[][] children = new int[P][21];
+        int[] count = new int[P];
+        
+        for (int i = 0; i < P; i++) {
+            st = new StringTokenizer(br.readLine());
+            children[i][0] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j < 21; j++) {
+                children[i][j] = Integer.parseInt(st.nextToken());
+                for (int k = 1; k < j; k++) {
+                    if (children[i][k] > children[i][j]) {
+                        count[i]++;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < P; i++) {
+            System.out.println(i+1 + " " + count[i]);
+        }
+    }
+}
