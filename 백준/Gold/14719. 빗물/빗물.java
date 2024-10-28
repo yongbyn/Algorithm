6f5ec9f5ab3de1ef029d79c5540e0891ecdede01
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2024-10-28 10:42:55 +0900

    [Gold V] Title: 빗물, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int[] blocks = new int[W];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < W; i++) {
+			blocks[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int totalRain = 0;
+
+
+		for (int i = 0; i < W; i++) {
+			int left = 0;
+			int right = 0;
+
+			for (int j = 0; j < i; j++) {
+				left = Math.max(left, blocks[j]);
+			}
+
+			for (int j = i + 1; j < W; j++) {
+				right = Math.max(right, blocks[j]);
+			}
+
+			if (blocks[i] < left && blocks[i] < right) {
+				totalRain += Math.min(left, right) - blocks[i];
+			}
+
+		}
+
+		System.out.println(totalRain);
+	}
+}
