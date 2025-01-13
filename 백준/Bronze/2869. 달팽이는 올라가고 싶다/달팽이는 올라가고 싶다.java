a1379cc73a6f1c62d1e5c2be4a6e50673b951b98
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2025-01-13 21:23:14 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+
+		int day = (V - B) / (A - B);
+
+		day = (V - B) % (A - B) != 0? day + 1 : day;
+
+		System.out.println(day);
+	}
+}
