685c9169d8047c4a296d67bec8ee6d255fe47734
YongBin KIM <44955172+yongbyn@users.noreply.github.com>
2025-01-21 21:48:00 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		while(N != 1){
+            for(int i = 2; i <= N; i++){
+                if(N % i == 0) {
+                    System.out.println(i);
+                    N = N / i;
+                    break;
+                }
+            }
+        }
+	}
+}
